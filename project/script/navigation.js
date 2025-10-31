54787fef659f42f1a2cf9f4c658297fc302baa3c
BYU-WDD130 <henriquezdery@gmail.com>
2025-10-31 11:02:49 -0400

    project

--- project/script/navigation.js
+++ project/script/navigation.js
@@ -3,3 +3,8 @@ const navButton = document.querySelector('#ham-btn');
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('show');
 });
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+document.getElementById("LastModified").textContent = "Last Modified: " + document.lastModified;
+
